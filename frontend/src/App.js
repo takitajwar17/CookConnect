244b7795d6a55f825849d58cfa19818d053b6b41
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2024-07-09 19:21:20 +0600

    feat(frontend): implement responsive navigation menu with Material UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,24 @@ import {Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Create from './components/Create';
+import NavBar from './components/NavBar';
+
 
 
 function App() {
+  const myWidth = 220;
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
+      <NavBar 
+      drawerWidth={myWidth}
+      content = 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>  
+      />
     </div>
   );
 }
