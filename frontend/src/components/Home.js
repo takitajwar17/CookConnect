7341049c8f93f8b2edb7bc602f4b9de17f20baef
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2024-07-10 02:43:18 +0600

    feat: add recipe table to display all recipes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,81 @@
-import React from 'react'
+import { useMemo, useEffect, useState } from 'react';
+import AxiosInstance from './axios';
+import { MaterialReactTable,useMaterialReactTable } from 'material-react-table';
+import { Box, IconButton } from '@mui/material';
+import { Edit as EditIcon, Delete as DeleteIcon } from '@mui/icons-material';
 
 const Home = () => {
+  const [myData, setMyData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getData = () => {
+    AxiosInstance.get('recipe/')
+      .then((res) => {
+        setMyData(res.data);
+        console.log(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'actions', 
+        header: 'Actions',
+        size: 50,
+        Cell: ({ row }) => (
+          <Box sx={{ display: 'flex', flexWrap: 'nowrap', gap: '8px' }}>
+            <IconButton
+              color="secondary"
+              onClick={() => console.log('Edit', row.original)}
+            >
+              <EditIcon />
+            </IconButton>
+            <IconButton
+              color="error"
+              onClick={() => console.log('Delete', row.original)}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </Box>
+        ),
+      },
+      {
+        accessorKey: 'name', 
+        header: 'Recipe Title',
+        size: 150,
+      },
+      {
+        accessorKey: 'ingredients', 
+        header: 'Ingredients',
+        size: 150,
+      },
+      {
+        accessorKey: 'instructions',
+        header: 'Instructions',
+        size: 200,
+      },
+    ],
+    [],
+  );
+
   return (
     <div>
-      Home
+      {loading ? <h2>Loading...</h2> : 
+        <MaterialReactTable 
+          columns={columns} 
+          data={myData} 
+        />}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
