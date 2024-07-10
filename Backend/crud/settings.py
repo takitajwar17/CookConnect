030e5406da5e2ba5d6929056ef98b05e29d997c5
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2024-07-10 20:28:07 +0600

    feat(backend): set up RDS connection for database in AWS

--- Backend/crud/settings.py
+++ Backend/crud/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'initialcookconnectdb',
+        'USER': 'mysuperuser',
+        'PASSWORD': 'mysuperuser',
+        'HOST': 'cookconnectdb.ch800mq0m4yu.us-west-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
